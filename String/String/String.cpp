47656a5843e71ca602cb67604e0ddf3e48911c40
SasiKoka <borja.sasieta@gmail.com>
2018-10-06 17:03:14 +0200

    fets els constructors, els getters i els setters
    fet, ara queda strcat, strcopy, strcomp, strlen i clear

--- String/String/String.cpp
+++ String/String/String.cpp
@@ -4,4 +4,14 @@ String::String() : string(""), mem_allocate(sizeof(char*)) {}
 
 String::String(char* s) : string(s), mem_allocate(sizeof(s)) {}
 
+String::String(String &s) : string(s.getString()), mem_allocate(s.getMemAlloc()) {}
+
 String::~String(){}
+
+char* String::getString() { return this->string; }
+
+void String::setString(String &s) { this->string = s.getString(); this->mem_allocate = s.getMemAlloc(); }
+
+unsigned int String::getMemAlloc() { return this->mem_allocate; }
+
+void String::setMemAlloc(unsigned int mem) { this->mem_allocate = mem; }
--- String/String/String.h
+++ String/String/String.h
@@ -2,10 +2,17 @@
 class String
 {
 public:
+	//constructors
 	String();
 	String(char* s);
+	String(String &s);
+	//destructors
 	virtual ~String();
-
+	//getters i setters
+	char* getString();
+	void setString(String &s);
+	unsigned int getMemAlloc();
+	void setMemAlloc(unsigned int mem);
 private:
 	char* string;
 	unsigned int mem_allocate;
