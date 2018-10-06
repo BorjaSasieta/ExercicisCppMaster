a109885034b23c59eb7f6f43b5f4c0042f098b97
SasiKoka <borja.sasieta@gmail.com>
2018-10-06 14:59:06 +0200

    fet el metode de sumes de vectors
    Fet el metode eixe, també s'han afegit els getters i els setters

--- Vector3/Vector3/Vector3.cpp
+++ Vector3/Vector3/Vector3.cpp
@@ -20,3 +20,13 @@ Vector3::Vector3(Vector3 &vec3) {
 Vector3::~Vector3()
 {
 }
+
+int Vector3::getX() { return x; }
+int Vector3::getY() { return y; }
+int Vector3::getZ() { return z; }
+
+void Vector3::setX(int x) { this->x = x; }
+void Vector3::setY(int y) { this->y = y; }
+void Vector3::setZ(int z) { this->z = z; }
+
+Vector3 Vector3::plusVector(Vector3 &a, Vector3 &b) { return Vector3((a.getX() - b.getX()), (a.getY() - b.getY()), (a.getZ() - b.getZ())); }
--- Vector3/Vector3/Vector3.h
+++ Vector3/Vector3/Vector3.h
@@ -2,15 +2,25 @@
 class Vector3
 {
 public:
+	//constructors
 	Vector3();
 	Vector3(int x, int y, int z);
 	Vector3(Vector3 &vec3);
+	//destructors
 	virtual ~Vector3();
+	//getters y setters
+	int getX();
+	int getY();
+	int getZ();
+	void setX(int x);
+	void setY(int y);
+	void setZ(int z);
+	//altres metodes
+	Vector3 plusVector(Vector3 &a, Vector3 &b);
 
 private:
 	int x = 0;
 	int y = 0;
 	int z = 0;
-
 };
 
