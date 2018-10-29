f329450f375ee7b331f5ee90081a3d952086db30
Borja <borja.sasieta@gmail.com>
2018-10-29 19:23:15 +0100

    corregido
    en teoria bien

--- Vector3/Vector3/Vector3.cpp
+++ Vector3/Vector3/Vector3.cpp
@@ -5,13 +5,13 @@ Vector3::Vector3(){}
 
 Vector3::Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 
-Vector3::Vector3(Vector3 &vec3) : x(vec3.x), y(vec3.y), z(vec3.z) {}
+Vector3::Vector3(const Vector3 &vec3) : x(vec3.x), y(vec3.y), z(vec3.z) {}
 
 Vector3::~Vector3(){}
 
-float Vector3::getX() { return this->x; }
-float Vector3::getY() { return this->y; }
-float Vector3::getZ() { return this->z; }
+const float Vector3::getX() { return this->x; }
+const float Vector3::getY() { return this->y; }
+const float Vector3::getZ() { return this->z; }
 
 void Vector3::setX(int x) { this->x = x; }
 void Vector3::setY(int y) { this->y = y; }
@@ -20,10 +20,12 @@ void Vector3::setZ(int z) { this->z = z; }
 Vector3 Vector3::plusVector(Vector3 a) { return Vector3((a.getX() + this->x), (a.getY() + this->y), (a.getZ() + this->z)); }
 
 void Vector3::normalize() { 
-	double operand = sqrt(pow(this->x,2) + pow(this->y, 2) + pow(this->z, 2));
+	double operand = sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
 	this->x /= operand;
 	this->y /= operand;
 	this->z /= operand;
 }
 
-Vector3 Vector3::distance(Vector3 v) { return Vector3((v.getX() - this->x), (v.getY() - this->y), (v.getZ() - this->z)); }
+float Vector3::distance(Vector3 const v) { return sqrt((v.x - this->x) * (v.x - this->x) + 
+									(v.y - this->y) * (v.y - this->y) + 
+									(v.z - this->z) * (v.z - this->z)); }
--- Vector3/Vector3/Vector3.h
+++ Vector3/Vector3/Vector3.h
@@ -6,20 +6,20 @@ public:
 	//constructors
 	Vector3();
 	Vector3(float x, float y, float z);
-	Vector3(Vector3 &vec3);
+	Vector3(const Vector3 &vec3);
 	//destructors
 	virtual ~Vector3();
 	//getters y setters
-	float getX();
-	float getY();
-	float getZ();
+	const float getX();
+	const float getY();
+	const float getZ();
 	void setX(int x);
 	void setY(int y);
 	void setZ(int z);
 	//altres metodes
 	Vector3 plusVector(Vector3 a);
 	void normalize();
-	Vector3 distance(Vector3 v);
+	float distance(Vector3 const v);
 private:
 	float x = 0.0f;
 	float y = 0.0f;
